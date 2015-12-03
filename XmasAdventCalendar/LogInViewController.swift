05e43ae30831e1ace410cafac443b8bad006a840
Karen L Irvine <kirvine@andrew.cmu.edu>
2015-12-03 16:41:06 -0500

    problems connecting sign up view

--- XmasAdventCalendar/LogInViewController.swift
+++ XmasAdventCalendar/LogInViewController.swift
@@ -12,30 +12,108 @@ import ParseUI
 
 class LoginViewController : PFLogInViewController {
     
-    var backgroundImage : UIImageView!;
+    var backgroundImage : UIImageView!
+    var viewsToAnimate: [UIView!]!
+    var viewsFinalYPosition : [CGFloat]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // set custom background image
-        backgroundImage = UIImageView(image: UIImage(named: "hanging"))
-        backgroundImage.contentMode = UIViewContentMode.ScaleAspectFill
-        self.logInView!.insertSubview(backgroundImage, atIndex: 0)
+        customizeBackground()
         
-        // remove the parse Logo
-        let logo = UILabel()
-        logo.text = "Christmas Advent Calendar"
-        logo.textColor = UIColor.whiteColor()
-        logo.font = UIFont(name: "Lights", size: 40)
-        logo.shadowColor = UIColor.lightGrayColor()
-        logo.shadowOffset = CGSizeMake(2, 2)
-        logInView?.logo = logo
+        // custom logo
+        customizeLogo()
+        
+        // customize login button
+        customizeLogIn()
+        
+        // customize forgot password
+        logInView?.passwordForgottenButton?.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+
+        // customize sign up button
+        customizeSignUp(logInView?.signUpButton!)
+        
+        // create an array of all the views we want to animate at launch
+        viewsToAnimate = [self.logInView?.usernameField, self.logInView?.passwordField, self.logInView?.logInButton]
+
+        // set signUpViewController
+        self.signUpController = SignUpViewController()
+
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         // stretch background image to fill screen
         backgroundImage.frame = CGRectMake( 0,  0,  self.logInView!.frame.width,  self.logInView!.frame.height)
+        
+        // position logo at top
+        positionLogo()
+        
+        // position views initially
+        positionViews()
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // animate views to float up
+        if viewsFinalYPosition.count == self.viewsToAnimate.count {
+            UIView.animateWithDuration(1, delay: 0.0, options: .CurveEaseInOut,  animations: { () -> Void in
+                for viewToAnimate in self.viewsToAnimate {
+                    let currentFrame = viewToAnimate.frame
+                    viewToAnimate.frame = CGRectMake(currentFrame.origin.x, self.viewsFinalYPosition.removeAtIndex(0), currentFrame.width, currentFrame.height)
+                }
+            }, completion: nil)
+        }
+    }
+    
+    //  MARK:   Customizations
+    
+    func customizeBackground() {
+        backgroundImage = UIImageView(image: UIImage(named: "snow3"))
+        backgroundImage.contentMode = UIViewContentMode.ScaleAspectFill
+        self.logInView!.insertSubview(backgroundImage, atIndex: 0)
+    }
+    
+    func customizeLogo() {
+        let logo = UILabel()
+        logo.text = "Advent Calendar"
+        logo.textColor = UIColor.whiteColor()
+        logo.font = UIFont(name: "Pacifico", size: 42)
+        logo.shadowColor = UIColor.lightGrayColor()
+        logo.shadowOffset = CGSizeMake(2, 2)
+        logInView?.logo = logo
+    }
+    
+    func customizeLogIn() {
+        logInView?.logInButton?.setTitleColor(UIColor.grayColor(), forState: .Normal)
+        logInView?.logInButton?.setBackgroundImage(nil, forState: .Normal)
+        logInView?.logInButton?.backgroundColor = UIColor.whiteColor()
+    }
+    
+    func customizeSignUp(button: UIButton!) {
+        button.setBackgroundImage(nil, forState: .Normal)
+        button.backgroundColor = UIColor.clearColor()
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.whiteColor().CGColor
+    }
+    
+    func positionLogo() {
+        logInView!.logo!.sizeToFit()
+        let logoFrame = logInView!.logo!.frame
+        logInView!.logo!.frame = CGRectMake(logoFrame.origin.x, logInView!.usernameField!.frame.origin.y - logoFrame.height - 16, logInView!.frame.width,  logoFrame.height)
+    }
+    
+    func positionViews() {
+        viewsFinalYPosition = [CGFloat]()
+        for viewToAnimate in viewsToAnimate {
+            let currentFrame = viewToAnimate.frame
+            viewsFinalYPosition.append(currentFrame.origin.y)
+            viewToAnimate.frame = CGRectMake(currentFrame.origin.x, self.view.frame.height + currentFrame.origin.y, currentFrame.width, currentFrame.height)
+        }
     }
     
 }
--- XmasAdventCalendar/SignUpViewController.swift
+++ XmasAdventCalendar/SignUpViewController.swift
@@ -10,99 +10,24 @@ import UIKit
 import Parse
 import ParseUI
 
-class SignUpViewController: UIViewController {
-
-    @IBOutlet weak var emailField: UITextField!
-    @IBOutlet weak var usernameField: UITextField!
-    @IBOutlet weak var passwordField: UITextField!
+class SignUpViewController : PFSignUpViewController {
     
-    var actInd : UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
-
+    var backgroundImage : UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // style activity indicator
-        self.actInd.center = self.view.center
-        self.actInd.hidesWhenStopped = true
-        self.actInd.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
         
-        view.addSubview(self.actInd)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // set custom background image
+        customizeBackground()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //  MARK:   Customizations
+    
+    func customizeBackground() {
+        backgroundImage = UIImageView(image: UIImage(named: "snow3"))
+        backgroundImage.contentMode = UIViewContentMode.ScaleAspectFill
+        self.signUpView!.insertSubview(backgroundImage, atIndex: 0)
     }
-    */
-    
-    // MARK:    Actions
-    
-    @IBAction func signUpAction(sender: AnyObject) {
-        var email = self.emailField.text
-        var username = self.usernameField.text
-        var password = self.passwordField.text
-        
-        if (username?.utf16.count < 4 || password?.utf16.count < 6) {
-            
-            var alert = UIAlertView(title: "Invalid", message: "Username must have 4 or more characters and password must be greater than 6 characters", delegate: self, cancelButtonTitle: "OK")
-            
-        } else {
-            
-            self.actInd.startAnimating()
-            
-            PFUser.logInWithUsernameInBackground(username as String!, password: password as String!, block: { (user, error) -> Void in
-                
-                self.actInd.stopAnimating()
-                
-                if ((user) != nil) {
-                    
-                    var alert = UIAlertView(title: "Success", message: "Logged In", delegate: self, cancelButtonTitle: "OK")
-                    
-                } else if (email?.utf16.count < 8) {
-                    
-                    var alert = UIAlertView(title: "Invalid", message: "Please enter a valid email.", delegate: self, cancelButtonTitle: "OK")
-                    
-                } else {
-                    
-                    self.actInd.startAnimating()
-                    
-                    var newUser = PFUser()
-                    
-                    newUser.email = email
-                    newUser.username = username
-                    newUser.password = password
-                    
-                    newUser.signUpInBackgroundWithBlock({ (succeed, error) -> Void in
-                        
-                        self.actInd.stopAnimating()
-                        
-                        if ((error) != nil) {
-                            var alert = UIAlertView(title: "Error", message: "\(error)", delegate: self, cancelButtonTitle: "OK")
-                            alert.show()
 
-                        } else {
-                            var alert = UIAlertView(title: "Success", message: "Signed Up!", delegate: self, cancelButtonTitle: "OK")
-                            alert.show()
-                        }
-                        
-                    })
-                    
-                }
-                
-            })
-            
-        }
-        
-    }
-    
+
 }
